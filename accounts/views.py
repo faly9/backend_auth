67280f80139447dfa3f0ebb88fe981ba91a0c45b
faly9 <randriantsoafaly8@gmail.com>
2026-03-19 18:40:19 +0000

    branche faly

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+#redirection vers la page de dashbord,juste recuperer le token via le url dans le lien de la validation
+from django.shortcuts import redirect
 # importation de l'user
 from .models import User
 from django.views.decorators.csrf import csrf_exempt
@@ -7,7 +9,7 @@ from rest_framework.decorators import api_view
 # token
 from rest_framework_simplejwt.tokens import RefreshToken
 # login and sing up
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login , get_user_model
 # SEND EMAIL
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import send_mail
@@ -18,77 +20,76 @@ import json
 # OTP 
 import random
 from django.utils import timezone
-# show peaple
+from datetime import timedelta
+# show people
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .serializers import UserSerializer
 
 from rest_framework.response import Response
+User = get_user_model()
 
-@api_view(["GET"])
-def hello_api(request):
-    return Response({"message": "Salut depuis Django !"})
-
+# REGISTER
 @api_view(['POST'])
 def register_view(request):
-    try:
-        data = request.data
-        username = data.get('username')
-        email = data.get('email')
-        password = data.get('password')
-
-        if not email:
-            return JsonResponse({'error': 'Email must be set'}, status=400)
-
-        if User.objects.filter(email=email).exists():
-            return JsonResponse({'error': 'Email already exists'}, status=400)
-
-        if User.objects.filter(username=username).exists():
-            return JsonResponse({'error': 'Username already exists'}, status=400)
-
-        user = User.objects.create_user(email=email, username=username, password=password)
-        user.is_active = False
-        user.save()
-
-        # génération token
-        uid = urlsafe_base64_encode(force_bytes(user.pk))
-        token = default_token_generator.make_token(user)
-
-        activation_link = f"http://127.0.0.1:8000/accounts/activate/{uid}/{token}/"
-
-        send_mail(
-            "Activation de votre compte",
-            f"Bonjour {username}, cliquez sur ce lien pour activer votre compte:\n{activation_link}",
-            None,
-            [email],
-            fail_silently=False
-        )
-
-        return JsonResponse({
-            'message': 'Utilisateur créé. Vérifiez votre email pour activer votre compte.'
-        }, status=201)
-
-    except json.JSONDecodeError:
-        return JsonResponse({'error': 'Invalid JSON'}, status=400)
-
-
-@api_view(['POST'])
-def login_view(request):
-
     data = request.data
+
+    username = data.get('username')
     email = data.get('email')
     password = data.get('password')
+
+    if not email:
+        return JsonResponse({'error': 'Email requis'}, status=400)
+
+    if User.objects.filter(email=email).exists():
+        return JsonResponse({'error': 'Email déjà utilisé'}, status=400)
+
+    if User.objects.filter(username=username).exists():
+        return JsonResponse({'error': 'Username déjà utilisé'}, status=400)
+
+    user = User.objects.create_user(
+        email=email,
+        username=username,
+        password=password
+    )
+    user.is_active = False
+    user.save()
+
+    # génération lien activation
+    uid = urlsafe_base64_encode(force_bytes(user.pk))
+    token = default_token_generator.make_token(user)
+
+    activation_link = f"http://127.0.0.1:8000/accounts/activate/{uid}/{token}/"
+
+    send_mail(
+        "Activation de votre compte",
+        f"Bonjour {username}, cliquez ici : {activation_link}",
+        None,
+        [email],
+    )
+
+    return JsonResponse({
+        'message': 'Compte créé. Vérifiez votre email.'
+    }, status=201)
+
+
+
+# LOGIN 
+@api_view(['POST'])
+def login_view(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
 
     user = authenticate(request, username=email, password=password)
 
     if user is None:
-        return JsonResponse({"message": "Identifiants invalides"}, status=401)
+        return JsonResponse({"error": "Identifiants invalides"}, status=401)
 
     if not user.is_active:
-        return JsonResponse({"message": "Compte non activé"}, status=403)
+        return JsonResponse({"error": "Compte non activé"}, status=403)
 
-    # génération OTP
+    # 🔥 OTP
     otp = str(random.randint(100000, 999999))
 
     user.otp = otp
@@ -97,19 +98,19 @@ def login_view(request):
 
     send_mail(
         "Votre code OTP",
-        f"Votre code de connexion est : {otp}",
+        f"Votre code est : {otp}",
         None,
         [user.email],
-        fail_silently=False
     )
 
     return JsonResponse({
-        "message": "OTP envoyé à votre email"
+        "message": "OTP envoyé"
     })
 
+
+# VERIFY OTP 
 @api_view(['POST'])
 def verify_otp(request):
-
     email = request.data.get("email")
     otp = request.data.get("otp")
 
@@ -121,30 +122,43 @@ def verify_otp(request):
     if user.otp != otp:
         return JsonResponse({"error": "OTP invalide"}, status=400)
 
+    if timezone.now() > user.otp_created_at + timedelta(minutes=5):
+        return JsonResponse({"error": "OTP expiré"}, status=400)
+
+    user.otp = None
+    user.save()
+
+    # 🔥 JWT
     refresh = RefreshToken.for_user(user)
 
     return JsonResponse({
         "refresh": str(refresh),
         "access": str(refresh.access_token),
-        "id" : user.id,
-        "email": user.email
     })
 
-@csrf_exempt
+# ACTIVATE ACCOUNT 
 def activate_account(request, uidb64, token):
     try:
         uid = urlsafe_base64_decode(uidb64).decode()
         user = User.objects.get(pk=uid)
-    except:
+    except (TypeError, ValueError, OverflowError, ObjectDoesNotExist):
         user = None
 
-    if user and default_token_generator.check_token(user, token):
+    if user and not user.is_active and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
-        return JsonResponse({"message": "Compte activé avec succès !"})
 
-    return JsonResponse({"error": "Lien invalide"})
+        # JWT
+        refresh = RefreshToken.for_user(user)
+        access = str(refresh.access_token)
+        refresh = str(refresh)
 
+        # REDIRECTION VERS REACT
+        return redirect(
+            f"http://localhost:5173/activate-success?access={access}&refresh={refresh}"
+        )
+
+    return redirect("http://localhost:5173/activate-error")
 
 # afficher tous les users de la base afin d'envoyer des message
 class UserListView(APIView):
